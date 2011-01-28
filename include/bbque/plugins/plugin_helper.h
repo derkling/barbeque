2bf299d676c6865a1dc94d75523bbe0047085eb1
Patrick Bellasi <derkling@gmail.com>
2011-01-28 14:55:11 +0100

    Add helper classes for plugins registration
    This patch provide a couple of helper classes to support registration of
    both dynamic and static plugins.
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

---
+++ include/bbque/plugins/plugin_helper.h
@@ -0,0 +1,99 @@
+/**
+ *       @file  plugin_helper.h
+ *      @brief  This provides an helper class that takes the drudge out of
+ *				writing the plugin glue code
+ *
+ * It is designed to work with plugin object classes that implement the
+ * PF_CreateFunc and PF_DestroyFunc mandatory functions as static methods.
+ *
+ *     @author  Patrick Bellasi (derkling), derkling@google.com
+ *
+ *   @internal
+ *     Created  01/27/2011
+ *    Revision  $Id: doxygen.templates,v 1.3 2010/07/06 09:20:12 mehner Exp $
+ *    Compiler  gcc/g++
+ *     Company  Politecnico di Milano
+ *   Copyright  Copyright (c) 2011, Patrick Bellasi
+ *
+ * This source code is released for free distribution under the terms of the
+ * GNU General Public License as published by the Free Software Foundation.
+ * =====================================================================================
+ */
+
+#ifndef BBQUE_PLUGIN_HELPER_H_
+#define BBQUE_PLUGIN_HELPER_H_
+
+#include "bbque/plugins/plugin.h"
+
+#error MUST include headers defining ASSERTS and CHECK
+
+namespace bbque { namespace plugins {
+
+class PluginHelper {
+
+	struct RegisterParams : public PF_RegisterParams {
+		RegisterParams(PF_PluginAPI_Version v,
+				PF_CreateFunc cf,
+				PF_DestroyFunc df,
+				PF_ProgrammingLanguage pl) {
+
+			version = v;
+			createFunc = cf;
+			destroyFunc = df;
+			programmingLanguage = pl;
+
+		}
+	};
+
+public:
+
+	PluginHelper(const PF_PlatformServices * _params) :
+		params(_params),
+		result(ExitPlugin) {
+	}
+
+	PF_ExitFunc GetResult() {
+		return result;
+	}
+
+	template <typename T>
+	void registerObject(const char * object_type,
+		PF_ProgrammingLanguage pl = PF_LANG_CPP) {
+		PF_PluginAPIVersion v = {1, 0};
+
+		// Version check
+		try {
+			CHECK (params_->version.major >= v.major)
+				<< "Version mismatch. PluginManager version must be "
+				<< "at least " << v.major << "." << v.minor;
+
+			RegisterParams rp(v, T::create, T::destroy, pl);
+			int32_t rc = params->registerObject(object_type, &rp);
+
+			CHECK (rc >= 0)
+				<< "Registration of object type "
+				<< objectType << "failed. "
+				<< "Error code=" << rc;
+		} catch (...) {
+			result_ = NULL;
+		}
+	}
+
+	static int32_t ExitPlugin() {
+		return 0;
+	}
+
+private:
+
+	const PF_PlatformServices * params;
+
+	PF_ExitFunc result;
+
+};
+
+} // namespace plugins
+
+} // namespace bbque
+
+#endif // BBQUE_PLUGIN_HELPER_H_
+
---
+++ include/bbque/plugins/static_plugin.h
@@ -0,0 +1,48 @@
+/**
+ *       @file  static_plugin.h
+ *      @brief  A static plugin wrapper class
+ *
+ * This class provides a simple mechanism that lets static plugins register
+ * their objects automatically with the PluginManager without requiring the
+ * application to explicitly initialize them. The way it works (when it
+ * works) is that the plugin defines a global instance of the StaticPlugin
+ * and passes it its initialization function (with a signature that matches
+ * PF_InitFunc). The StaticPlugin simply calls the
+ * PluginManager::InitializePlugin() method that ignites the static plugin
+ * initialization just like with dynamic plugins after loading the dynamic
+ * library.
+ *
+ *     @author  Patrick Bellasi (derkling), derkling@google.com
+ *
+ *   @internal
+ *     Created  01/27/2011
+ *    Revision  $Id: doxygen.templates,v 1.3 2010/07/06 09:20:12 mehner Exp $
+ *    Compiler  gcc/g++
+ *     Company  Politecnico di Milano
+ *   Copyright  Copyright (c) 2011, Patrick Bellasi
+ *
+ * This source code is released for free distribution under the terms of the
+ * GNU General Public License as published by the Free Software Foundation.
+ * =====================================================================================
+ */
+
+#ifndef BBQUE_STATIC_PLUGIN_H_
+#define BBQUE_STATIC_PLUGIN_H_
+
+#include "bbque/plugin_manager.h"
+
+namespace bbque { namespace plugins {
+
+class StaticPlugin {
+public:
+  StaticPlugin(PF_InitFunc initFunc) {
+    PluginManager::InitializePlugin(initFunc);
+  }
+};
+
+} // namespace plugins
+
+} // namespace bbque
+
+#endif // BBQUE_STATIC_PLUGIN_H_
+
