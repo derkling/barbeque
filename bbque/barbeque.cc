195a3b74ac38648eac3f560c113a7bc6b2e81b4e
Patrick Bellasi <derkling@gmail.com>
2011-03-23 19:56:24 +0100

    Added (dummy) testing framework support
    This patch introduces a simple while effective testing framework.
    We use test modules, i.e. plugins registering object within the "test."
    namespace, to run testcases.
    
    Once Barbeque starts it looks for modules with name starting by "test.",
    these must be modules implementing the TestIF. If at least one of such
    modules is found within the plugins folder, than Barbeque enters the
    "testing mode". In this mode, all testing modules are executed, one at
    each time, by calling the Test method they provide.
    Once all tests have been executed, Barbeque simply exits.
    
    To support this behavior, we could not link statically any module
    implementing the TestIF interface, otherwise Barbeque will never enter
    its main execution mode. For this reason, the static compilation of the
    dummy test module is not disabled.
    
    Whenever this simple approach should turn out to be too much simpler or
    restrictive, we will provide a better interface for testing support.
    
    NOTE: The folder containing testing modules could be defined using the "-p"
    command line switch. By default, this is the system folder: thus
    _ensure_ not to have testing modules within the system folder whenever
    Barbeque is deployed on a target system.
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

--- bbque/barbeque.cc
+++ bbque/barbeque.cc
@@ -24,6 +24,7 @@
 #include "bbque/platform_services.h"
 #include "bbque/plugin_manager.h"
 #include "bbque/resource_manager.h"
+#include "bbque/modules_factory.h"
 
 #include "bbque/utils/timer.h"
 #include "bbque/utils/utility.h"
@@ -32,10 +33,45 @@ namespace bb = bbque;
 namespace bp = bbque::plugins;
 namespace bu = bbque::utils;
 
+
 #define FMT(fmt) BBQUE_FMT(COLOR_GREEN, "BQ", fmt)
+
 
 /* The global timer, this can be used to get the time since Barbeque start */
 bu::Timer bbque_tmr(true);
+
+bool runTests(bp::PluginManager & pm) {
+	const bp::PluginManager::RegistrationMap & rm = pm.GetRegistrationMap();
+	bp::PluginManager::RegistrationMap::const_iterator near_match =
+		rm.lower_bound("test.");
+
+	if (near_match == rm.end() ||
+			((*near_match).first.compare(0,5,"test.")))
+		return false;
+
+	fprintf(stdout, FMT(".:: Entering Testing Mode\n"));
+
+	do {
+		bu::Timer test_tmr;
+
+		fprintf(stdout, "\n"FMT("___ Testing [%s]...\n"), (*near_match).first.c_str());
+
+		bp::TestIF * tms = bb::ModulesFactory::GetTestModule((*near_match).first);
+
+		test_tmr.start();
+		tms->Test();
+		test_tmr.stop();
+
+		fprintf(stdout, FMT("___ completed, [%11.6f]s\n"), test_tmr.getElapsedTime());
+
+		near_match++;
+
+	} while (near_match != rm.end() &&
+			((*near_match).first.compare(0,5,"test.")) == 0 );
+
+	fprintf(stdout, "\n\n"FMT(".:: All tests completed\n\n"));
+	return true;
+}
 
 int main(int argc, char *argv[]) {
 
@@ -58,6 +94,10 @@ int main(int argc, char *argv[]) {
 				cm.GetPluginsDir().c_str());
 		pm.LoadAll(cm.GetPluginsDir());
 	}
+
+	// Check if we have tests to run (i.e. "test." objects have been registered)
+	if (runTests(pm))
+		return EXIT_SUCCESS;
 
 	// Let's start baking applications...
 	bb::ResourceManager::GetInstance().Go();
