ce55e22b1756d594b8c0d7369e1fe336d2e1e9f8
Patrick Bellasi <derkling@gmail.com>
2011-04-22 19:52:21 +0200

    Added RPC Proxy class
    The RPCProxy class implements the proxy pattern on the modules
    implementing the RPCChannelIF interface. The purpose of this class is to
    provide message queue management, to support a priority based management
    of incoming application messages,and an asynchronous processing
    execution context.
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

---
+++ bbque/rpc_proxy.cc
@@ -0,0 +1,189 @@
+/**
+ *       @file  rpc_proxy.cc
+ *      @brief  The proxy interface to the Barbeque communication channel IF
+ *
+ * This defines the a proxy class use to provide message queuing support by
+ * wrapping the RPCChannelIF. This class unloads the channel modules
+ * from the message queuing management, thus allowing for a simpler
+ * implementaton. Meanwhile, this is class provides all the code to manage
+ * message queuing and dequenung policies.
+ *
+ *     @author  Patrick Bellasi (derkling), derkling@google.com
+ *
+ *   @internal
+ *     Created  01/28/2011
+ *    Revision  $Id: doxygen.templates,v 1.3 2010/07/06 09:20:12 mehner Exp $
+ *    Compiler  gcc/g++
+ *     Company  Politecnico di Milano
+ *   Copyright  Copyright (c) 2011, Patrick Bellasi
+ *
+ * This source code is released for free distribution under the terms of the
+ * GNU General Public License as published by the Free Software Foundation.
+ * =============================================================================
+ */
+
+#include "bbque/rpc_proxy.h"
+
+#include "bbque/modules_factory.h"
+#include "bbque/plugin_manager.h"
+#include "bbque/plugins/object_adapter.h"
+
+#include "bbque/plugins/rpc_channel_adapter.h"
+
+namespace bp = bbque::plugins;
+
+namespace bbque {
+
+/**
+ * Specialize the ObjectAdapter template for RPCChannel plugins
+ */
+typedef bp::ObjectAdapter<bp::RPCChannelAdapter, C_RPCChannel>
+	RPCChannel_ObjectAdapter;
+
+RPCProxy::RPCProxy(std::string const &id) :
+	done(false) {
+
+	// Get a logger
+	plugins::LoggerIF::Configuration conf(RPC_CHANNEL_NAMESPACE"prx");
+	logger = ModulesFactory::GetLoggerModule(std::cref(conf));
+	assert(logger!=NULL);
+
+	// Build a object adapter for the Logger
+	logger->Debug("RPC Proxy: RPC channel loading...");
+	RPCChannel_ObjectAdapter rcoa;
+	void* module = bp::PluginManager::GetInstance().
+						CreateObject(id, NULL, &rcoa);
+	if (!module) {
+		logger->Fatal("RPC Proxy: RPC channel load FAILED");
+		rpc_channel = std::unique_ptr<RPCChannelIF>();
+		return;
+	}
+
+	// Keep track of the low-level channel module
+	channelLoaded = true;
+	rpc_channel = std::unique_ptr<RPCChannelIF>((RPCChannelIF*)module);
+
+}
+
+RPCProxy::~RPCProxy() {
+	if (rpc_channel) {
+		//FIXME add plugins release code
+		//bp::PluginManager::GetInstance().
+		//	DeleteObject((void*)(rpc_channel.get()));
+	}
+}
+
+bool RPCProxy::channelLoaded = false;
+RPCProxy *RPCProxy::instance = NULL;
+RPCProxy *RPCProxy::GetInstance(std::string const & id) {
+
+	if (instance)
+		return instance;
+
+	instance = new RPCProxy(id);
+
+	if (!channelLoaded) {
+		delete instance;
+		return NULL;
+	}
+
+	return instance;
+}
+
+int RPCProxy::Init() {
+
+	assert(rpc_channel);
+	if (rpc_channel->Init())
+		return -1;
+
+	// Intialize message queues
+	logger->Debug("Using (dummy) message priority based on RPC message ID");
+
+	// Spawn the Enqueuing thread
+	msg_fetch_trd = std::thread(&RPCProxy::EnqueueMessages, this);
+
+	return 0;
+}
+
+size_t RPCProxy::RecvMessage(rpc_msg_ptr_t & msg) {
+	std::unique_lock<std::mutex> queue_status_ul(msg_queue_mtx);
+	channel_msg_t ch_msg;
+	size_t size;
+
+	if (!msg_queue.size()) {
+		// Wait for at least one element being pushed into the queue
+		logger->Debug("RPC Proxy: waiting for new message");
+		queue_ready_cv.wait(queue_status_ul);
+	}
+
+	// Dequeue received message
+	ch_msg = msg_queue.top();
+	msg_queue.pop();
+	queue_status_ul.unlock();
+
+	// Setup return values
+	msg = ch_msg.first;
+	size = ch_msg.second;
+
+	logger->Debug("RPC Proxy: got new message (size: %d)", size);
+
+	return size;
+}
+
+bp::RPCChannelIF::plugin_data_t RPCProxy::GetPluginData(
+		rpc_msg_ptr_t & msg) {
+	return rpc_channel->GetPluginData(msg);
+}
+
+void RPCProxy::ReleasePluginData(plugin_data_t & pd) {
+	return rpc_channel->ReleasePluginData(pd);
+}
+
+size_t RPCProxy::SendMessage(plugin_data_t & pd, rpc_msg_ptr_t & msg,
+							size_t count) {
+	return rpc_channel->SendMessage(pd, msg, count);
+}
+
+bool RPCProxy::RPCMsgCompare::operator() (const channel_msg_t & lhs,
+		const channel_msg_t & rhs) const {
+	const rpc_msg_ptr_t hdr1 = lhs.first;
+	const rpc_msg_ptr_t hdr2 = rhs.first;
+
+	// Dummy policy: give priority to responses (RPC_BBQ_* messages)
+	if (hdr1->msg_typ > hdr2->msg_typ)
+		return true;
+
+	return false;
+}
+
+void RPCProxy::EnqueueMessages() {
+	size_t size;
+	rpc_msg_ptr_t msg;
+	std::unique_lock<std::mutex> queue_status_ul(
+			msg_queue_mtx, std::defer_lock);
+
+	logger->Info("RPC Proxy: message fetcher started");
+	while (!done) {
+		// Wait for a new message being ready
+		size = rpc_channel->RecvMessage(msg);
+		assert(msg);
+
+		logger->Debug("RPC Proxy: new message, type: %d", msg->msg_typ);
+
+		// Enqueue the message
+		queue_status_ul.lock();
+		msg_queue.push(channel_msg_t(msg,size));
+		queue_status_ul.unlock();
+		queue_ready_cv.notify_one();
+
+		logger->Debug("RPC Proxy: new message queued (dim: %d)",
+				msg_queue.size());
+	}
+}
+
+void RPCProxy::FreeMessage(rpc_msg_ptr_t & msg) {
+	rpc_channel->FreeMessage(msg);
+}
+
+} // namespace bbque
+
---
+++ include/bbque/rpc_proxy.h
@@ -0,0 +1,213 @@
+/**
+ *       @file  rpc_proxy.h
+ *      @brief  The proxy interface to the Barbeque communication channel IF
+ *
+ * This defines the a proxy class use to provide message queuing support by
+ * wrapping the RPCChannelIF. This class unloads the channel modules
+ * from the message queuing management, thus allowing for a simpler
+ * implementaton. Meanwhile, this is class provides all the code to manage
+ * message queuing and dequenung policies.
+ *
+ *     @author  Patrick Bellasi (derkling), derkling@google.com
+ *
+ *   @internal
+ *     Created  01/28/2011
+ *    Revision  $Id: doxygen.templates,v 1.3 2010/07/06 09:20:12 mehner Exp $
+ *    Compiler  gcc/g++
+ *     Company  Politecnico di Milano
+ *   Copyright  Copyright (c) 2011, Patrick Bellasi
+ *
+ * This source code is released for free distribution under the terms of the
+ * GNU General Public License as published by the Free Software Foundation.
+ * =============================================================================
+ */
+
+#ifndef BBQUE_RPC_PROXY_H_
+#define BBQUE_RPC_PROXY_H_
+
+#include "bbque/plugins/rpc_channel.h"
+#include "bbque/plugins/logger.h"
+
+#include <memory>
+#include <mutex>
+#include <thread>
+#include <queue>
+
+using bbque::plugins::RPCChannelIF;
+
+namespace bbque {
+
+/**
+ * @brief Queuing support to the low-level communication interface.
+ */
+class RPCProxy : public RPCChannelIF {
+
+public:
+
+	/**
+	 * 
+	 */
+	static RPCProxy *GetInstance(std::string const & id);
+
+	/**
+	 * @brief Destructor
+	 */
+	~RPCProxy();
+
+	/**
+	 * @brief Initialize the communication channel.
+	 */
+	virtual int Init();
+
+	/**
+	 * @brief Get a pointer to the next message buffer.
+	 *
+	 * This methods blocks the caller until a new message is available and
+	 * then return a pointer to the beginning of the message buffer and the
+	 * size of the returned buffer.
+	 *
+	 * @note If more than one message are waiting to be processed, the message
+	 * returned depends on the dequeuing rules defined by the implementation
+	 * of this class.
+	 */
+	virtual size_t RecvMessage(rpc_msg_ptr_t & msg);
+
+	/**
+	 * @brief Get a pointer to plugins data.
+	 *
+	 * Based on the specified message buffer, the channel module could
+	 * allocate and initialize a set of plugin specific data. These data are
+	 * opaque to the Barbeque RTRM, but they will be passed to the plugin each
+	 * time a message should be sent.
+	 *
+	 * This method is called only after the reception of a RPC_EXC_PAIR
+	 * message, which is passed to the communication channel module as a
+	 * reference to map the new communication channel. A communication channel
+	 * module could use these information (or its own specific information
+	 * pre-pended to the specified message buffer) to setup a communication
+	 * channel with the application and save all the required handler to the
+	 * plugins data to be returned. These resource and correponding handler
+	 * will be passed to the module each time a new message should be sent to
+	 * an application.
+	 *
+	 * @see look at the BbqueRPC_FIFO_Server class for a usage example.
+	 *
+	 * @note this call is forwarded directly to the low-level channel module.
+	 */
+	virtual plugin_data_t GetPluginData(rpc_msg_ptr_t & msg);
+
+	/**
+	 * @brief Release plugins data.
+	 *
+	 * Ask the channel to release all the resources associated with the
+	 * specified plugin data. This Barbeque RTRM opaque type is usually used
+	 * to save the communication channel specific connection information. In
+	 * this case, this method authorize the communicaiton channel module to
+	 * close the corresponding connection and release all the resource.
+	 *
+	 * @see look at the BbqueRPC_FIFO_Server class for a usage example.
+	 *
+	 * @note this call is forwarded directly to the low-level channel module.
+	 */
+	virtual void ReleasePluginData(plugin_data_t & pd);
+
+	/**
+	 * @brief Send a message buffer to the specified application.
+	 *
+	 * This method blocks the caller until the specified message buffer could
+	 * be accepted for delivery to the specified application.
+	 *
+	 * @note this call is forwarded directly to the low-level channel module.
+	 */
+	virtual size_t SendMessage(plugin_data_t & pd, rpc_msg_ptr_t & msg,
+								size_t count);
+
+	/**
+	 * @brief Release the specified RPC message.
+	 *
+	 * This methods blocks the caller until a new message is available and
+	 * then return a pointer to the beginning of the message buffer and the
+	 * size of the returned buffer.
+	 */
+	virtual void FreeMessage(rpc_msg_ptr_t & msg);
+
+
+private:
+
+	/**
+	 * 
+	 */
+	static RPCProxy *instance;
+
+	static bool channelLoaded;
+
+	/**
+	 * 
+	 */
+	typedef std::pair<rpc_msg_ptr_t,size_t> channel_msg_t;
+
+	/**
+	 * 
+	 */
+	class RPCMsgCompare {
+	public:
+		bool operator() (const channel_msg_t & lhs,
+				const channel_msg_t & rhs) const;
+	};
+
+	/**
+	 * 
+	 */
+	bool done;
+
+	/**
+	 * @brief System logger instance
+	 */
+	plugins::LoggerIF *logger;
+
+	/**
+	 * 
+	 */
+	std::unique_ptr<RPCChannelIF> rpc_channel;
+
+	/**
+	 * 
+	 */
+	std::thread msg_fetch_trd;
+
+	/**
+	 * 
+	 */
+	std::priority_queue<channel_msg_t,
+		std::vector<channel_msg_t>,
+		RPCMsgCompare> msg_queue;
+
+	/**
+	 * 
+	 */
+	std::mutex msg_queue_mtx;
+
+	/**
+	 * 
+	 */
+	std::condition_variable queue_ready_cv;
+
+	/**
+	 * 
+	 */
+	RPCProxy(std::string const & id);
+
+	/**
+	 * @brief Enqueue a new received message.
+	 *
+	 * Provides an enqueuing thread which continuously fetch messages from the
+	 * low-level channel module and enqueue them to the proper queue.
+	 */
+	void EnqueueMessages();
+
+};
+
+} // namespace bbque
+
+#endif // BBQUE_RPC_PROXY_H_
+
