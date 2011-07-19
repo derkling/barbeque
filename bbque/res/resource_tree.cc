7d27bab38c21574e362409b757d35ed1ad327007
Giuseppe Massari <joe.massanga@gmail.com>
2011-07-19 14:34:40 +0200

    [ResourceTree] Code refactoring
    A refactoring of the implementation code has been performed in order to
    improve efficiency and readability.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- bbque/res/resource_tree.cc
+++ bbque/res/resource_tree.cc
@@ -48,108 +48,100 @@ ResourceTree::ResourceTree():
 
 
 ResourcePtr_t & ResourceTree::insert(std::string const & _rsrc_path) {
-	// Start from the root
-	ResourceNode * curr_node = root;
-
 	// Extract the first namespace level in the resource path
-	std::string ns_path	= _rsrc_path;
-	std::string curr_ns = SplitAndPop(ns_path);
+	ResourceNode * curr_node = root;
+	std::string ns_path(_rsrc_path);
+	std::string curr_ns(SplitAndPop(ns_path));
 
 	// For each namespace level...
-	while (!curr_ns.empty()) {
+	for (; !curr_ns.empty(); curr_ns = SplitAndPop(ns_path)) {
 
-		// If the resource has not children, create the first child using the
-		// current namespace level as name
-		 if (curr_node->children.empty()) {
+		// If the resource has not children create the first child using the
+		// current namespace level
+		if (curr_node->children.empty()) {
 			curr_node = add_child(curr_node, curr_ns);
-			// Update the max depth value
+
+			// Update max depth value
 			if (curr_node->depth > max_depth)
 				max_depth = curr_node->depth;
+			continue;
 		}
-		else {
-			// Children node iterators
-			ResourceNodesList_t::iterator it = curr_node->children.begin();
-			ResourceNodesList_t::iterator end = curr_node->children.end();
 
+		// Iterate over siblings
+		ResourceNodesList_t::iterator it(curr_node->children.begin());
+		ResourceNodesList_t::iterator end(curr_node->children.end());
+		for (; it != end; ++it) {
 			// Check if the current resource path level exists
-			for (; it != end; ++it) {
-				// Yes: move one level down
-				if ((*it)->data->Name().compare(curr_ns) == 0) {
-					curr_node = *it;
-					break;
-				}
-			}
-
-			// No: add a new resource as sibling node
-			if (it == end)
-				curr_node = add_child(curr_node, curr_ns);
+			if ((*it)->data->Name().compare(curr_ns) != 0)
+				continue;
+			// Yes: move one level down
+			curr_node = *it;
+			break;
 		}
 
-		// Pop next level/namespace in the path
-		curr_ns = SplitAndPop(ns_path);
+		// No: add a new resource as sibling node
+		if (it == end)
+			curr_node = add_child(curr_node, curr_ns);
 	}
+
 	// Return the new object just created
 	return curr_node->data;
 }
 
 
 bool ResourceTree::find_node(ResourceNode * curr_node,
-		std::string const & rsrc_path, SearchOption_t opt,
+		std::string const & rsrc_path,
+		SearchOption_t opt,
 		std::list<ResourcePtr_t> & matches) const {
 
-	// Null node check
-	if (curr_node == NULL)
+	// Null node / empty children lsit check
+	if ((!curr_node) || (curr_node->children.empty()))
 		return false;
 
 	// Extract the first node in the path, and save the remaining path string
-	std::string next_path = rsrc_path;
-	std::string curr_ns = SplitAndPop(next_path);
+	std::string next_path(rsrc_path);
+	std::string curr_ns(SplitAndPop(next_path));
+	if (curr_ns.empty())
+		return false;
 
-	// Parse the path
-	if (!curr_ns.empty()) {
-		// Stop search if children list is empty
-		if (curr_node->children.empty())
-			return 0;
+	// Children iterators
+	ResourceNodesList_t::iterator it_child(curr_node->children.begin());
+	ResourceNodesList_t::iterator end_child(curr_node->children.end());
 
-		// Children iterators
-		ResourceNodesList_t::iterator it_child = curr_node->children.begin();
-		ResourceNodesList_t::iterator end_child = curr_node->children.end();
+	// Check if the current namespace node exists looking for it in the
+	// list of children
+	for (; it_child != end_child; ++it_child) {
 
-		// Check if the current namespace node exists looking for it in the
-		// list of children
-		for (; it_child != end_child; ++it_child) {
+		// Check if the current namespace to find is ID-based
+		std::string res_name = (*it_child)->data->Name();
+		size_t id_pos = std::string::npos;
+		if (opt == RT_SET_MATCHES)
+			id_pos = curr_ns.find_first_of("0123456789");
 
-			// Check if the current namespace to find is ID-based
-			std::string res_name = (*it_child)->data->Name();
-			size_t id_pos = std::string::npos;
-			if (opt == RT_SET_MATCHES)
-				id_pos = curr_ns.find_first_of("0123456789");
-
-			// If the search is not ID based (path template search)
-			// remove the ID numbre from the current resource namespace
-			if ((opt == RT_FIRST_MATCH) || (opt == RT_ALL_MATCHES)
-				|| ((opt == RT_SET_MATCHES) && (id_pos == std::string::npos))) {
-				id_pos = res_name.find_first_of("0123456789");
-				res_name = res_name.substr(0, id_pos);
-			}
-
-			// Namespaces comparison
-			if (curr_ns.compare(res_name) == 0) {
-
-				// Matched. If we're at the end of the path, append the
-				// resource descriptor into the list to return. Otherwise
-				// continue recursively
-				if (next_path.empty())
-					matches.push_back((*it_child)->data);
-				else
-					find_node(*it_child, next_path, opt, matches);
-
-				// If the search doesn't require all the matches we can stop
-				if ((opt == RT_EXACT_MATCH) || (opt == RT_FIRST_MATCH))
-					break;
-			}
+		// If not (path template search) remove the ID number from the current
+		// resource namespace
+		if ((opt != RT_EXACT_MATCH) && (id_pos == std::string::npos)) {
+			id_pos = res_name.find_first_of("0123456789");
+			res_name = res_name.substr(0, id_pos);
 		}
+
+		// Namespaces comparison
+		if (curr_ns.compare(res_name) != 0)
+			continue;
+
+		// Matched. If we're at the end of the path, append the
+		// resource descriptor into the list to return.
+		if (next_path.empty())
+			matches.push_back((*it_child)->data);
+		else
+			// ... Otherwise continue recursively
+			find_node(*it_child, next_path, opt, matches);
+
+		// If the search doesn't require all the matches we can stop
+		if ((opt == RT_EXACT_MATCH) || (opt == RT_FIRST_MATCH))
+			break;
 	}
+
 	// Return true if the list is not empty
 	return !matches.empty();
 }
@@ -157,7 +149,6 @@ bool ResourceTree::find_node(ResourceNode * curr_node,
 
 ResourceNode * ResourceTree::add_child(ResourceNode * curr_node,
 		std::string const & rsrc_name) {
-
 	// Create the new resource node
 	ResourceNode * _node = new ResourceNode;
 	_node->data = ResourcePtr_t(new Resource(rsrc_name));
@@ -173,15 +164,12 @@ ResourceNode * ResourceTree::add_child(ResourceNode * curr_node,
 
 
 void ResourceTree::print_children(ResourceNode * _node, int _depth) {
-
 	// Increase the level of depth
 	++_depth;
 
-	// Children iterators
-	ResourceNodesList_t::iterator it = _node->children.begin();
-	ResourceNodesList_t::iterator end = _node->children.end();
-
 	// Print all the children
+	ResourceNodesList_t::iterator it(_node->children.begin());
+	ResourceNodesList_t::iterator end(_node->children.end());
 	for (; it != end; ++it) {
 		// Child name
 		for (int i= 0; i < _depth-1; ++i)
@@ -197,10 +185,9 @@ void ResourceTree::print_children(ResourceNode * _node, int _depth) {
 
 
 void ResourceTree::clear_node(ResourceNode * _node) {
-
 	// Children iterators
-	ResourceNodesList_t::iterator it = _node->children.begin();
-	ResourceNodesList_t::iterator end = _node->children.end();
+	ResourceNodesList_t::iterator it(_node->children.begin());
+	ResourceNodesList_t::iterator end(_node->children.end());
 
 	// Recursive clear
 	for (; it != end; ++it) {
