51ef1dee988c1bdc6eafadffc099c9132f8a3b89
Patrick Bellasi <derkling@gmail.com>
2011-09-06 19:12:13 +0200

    [RM] Add metrics collection
    This patch exploits the MetricsCollection suport to define and collect a
    set of metrics related to the ResourceManager module.
    So fare the metrics collected are:
    - the counting of Events (START, STOP, USR1 and USR2)
    - the counting of Scheduler activations
    - the counting of Scheduler failures causes
    - the counting of Synchronization activations
    - the processing time of Events processors (START, STOP, USR1 and USR2)
    - the period of Events (START, STOP, USR1 and USR2)
    - the period of Scheduler activations
    - the period of Synchronization acrivations
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

--- bbque/resource_manager.cc
+++ bbque/resource_manager.cc
@@ -28,7 +28,38 @@
 #include "bbque/application_manager.h"
 
 #include "bbque/utils/utility.h"
-#include "bbque/utils/timer.h"
+
+#define RESOURCE_MANAGER_NAMESPACE "bq.rm"
+
+/** Metrics (class COUNTER) declaration */
+#define RM_COUNTER_METRIC(NAME, DESC)\
+ {RESOURCE_MANAGER_NAMESPACE"."NAME, DESC, MetricsCollector::COUNTER, 0}
+/** Increase counter for the specified metric */
+#define RM_COUNT_EVENT(METRICS, INDEX) \
+	mc.Count(METRICS[INDEX].mh);
+/** Increase counter for the specified metric */
+#define RM_COUNT_EVENTS(METRICS, INDEX, AMOUNT) \
+	mc.Count(METRICS[INDEX].mh, AMOUNT);
+
+/** Metrics (class SAMPLE) declaration */
+#define RM_SAMPLE_METRIC(NAME, DESC)\
+ {RESOURCE_MANAGER_NAMESPACE"."NAME, DESC, MetricsCollector::SAMPLE, 0}
+/** Reset the timer used to evaluate metrics */
+#define RM_RESET_TIMING(TIMER) \
+	TIMER.start();
+/** Acquire a new time sample */
+#define RM_GET_TIMING(METRICS, INDEX, TIMER) \
+	mc.AddSample(METRICS[INDEX].mh, TIMER.getElapsedTimeMs());
+/** Acquire a new (generic) sample */
+#define RM_ADD_SAMPLE(METRICS, INDEX, SAMPLE) \
+	mc.AddSample(METRICS[INDEX].mh, SAMPLE);
+
+/** Metrics (class PERIDO) declaration */
+#define RM_PERIOD_METRIC(NAME, DESC)\
+ {RESOURCE_MANAGER_NAMESPACE"."NAME, DESC, MetricsCollector::PERIOD, 0}
+/** Acquire a new time sample */
+#define RM_GET_PERIOD(METRICS, INDEX, PERIOD) \
+	mc.PeriodSample(METRICS[INDEX].mh, PERIOD);
 
 namespace br = bbque::res;
 namespace bu = bbque::utils;
@@ -36,6 +67,43 @@ namespace bp = bbque::plugins;
 namespace po = boost::program_options;
 
 namespace bbque {
+
+/* Definition of metrics used by this module */
+MetricsCollector::MetricsCollection_t
+ResourceManager::metrics[RM_METRICS_COUNT] = {
+
+	//----- Event counting metrics
+	RM_COUNTER_METRIC("evt.tot",	"Total events"),
+	RM_COUNTER_METRIC("evt.start",	"  START events"),
+	RM_COUNTER_METRIC("evt.stop",	"  STOP  events"),
+	RM_COUNTER_METRIC("evt.usr1",	"  USR1  events"),
+	RM_COUNTER_METRIC("evt.usr2",	"  USR2  events"),
+
+	RM_COUNTER_METRIC("sch.tot",	"Total Scheduler activations"),
+	RM_COUNTER_METRIC("sch.failed",	"  FAILED  schedules"),
+	RM_COUNTER_METRIC("sch.delayed","  DELAYED schedules"),
+	RM_COUNTER_METRIC("sch.empty",	"  EMPTY   schedules"),
+
+	RM_COUNTER_METRIC("syn.tot",	"Total Synchronization activations"),
+
+	//----- Sampling statistics
+	RM_SAMPLE_METRIC("evt.avg.time",  "Avg events processing t[ms]"),
+	RM_SAMPLE_METRIC("evt.avg.start", "  START events"),
+	RM_SAMPLE_METRIC("evt.avg.stop",  "  STOP  events"),
+	RM_SAMPLE_METRIC("evt.avg.usr1",  "  USR1  events"),
+	RM_SAMPLE_METRIC("evt.avg.usr2",  "  USR2  events"),
+
+	RM_PERIOD_METRIC("evt.per",		"Avg events period t[ms]"),
+	RM_PERIOD_METRIC("evt.per.start",	"  START events"),
+	RM_PERIOD_METRIC("evt.per.stop",	"  STOP  events"),
+	RM_PERIOD_METRIC("evt.per.usr1",	"  USR1  events"),
+	RM_PERIOD_METRIC("evt.per.usr2",	"  USR2  events"),
+
+	RM_PERIOD_METRIC("sch.per",   "Avg Scheduler period t[ms]"),
+	RM_PERIOD_METRIC("syn.per",   "Avg Synchronization period t[ms]"),
+
+};
+
 
 ResourceManager & ResourceManager::GetInstance() {
 	static ResourceManager rtrm;
@@ -53,6 +121,8 @@ ResourceManager::ResourceManager() :
 	ra(br::ResourceAccounter::GetInstance()),
 	mc(bu::MetricsCollector::GetInstance()) {
 
+	//---------- Setup all the module metrics
+	mc.Register(metrics, RM_METRICS_COUNT);
 }
 
 ResourceManager::~ResourceManager() {
@@ -61,7 +131,7 @@ ResourceManager::~ResourceManager() {
 void ResourceManager::Setup() {
 
 	//---------- Get a logger module
-	std::string logger_name("bq.rm");
+	std::string logger_name(RESOURCE_MANAGER_NAMESPACE);
 	bp::LoggerIF::Configuration conf(logger_name.c_str());
 	logger = ModulesFactory::GetLoggerModule(std::cref(conf));
 	if (!logger) {
@@ -97,7 +167,8 @@ void ResourceManager::NotifyEvent(controlEvent_t evt) {
 void ResourceManager::Optimize() {
 	SynchronizationManager::ExitCode_t syncResult;
 	SchedulerManager::ExitCode_t schedResult;
-	bu::Timer optimization_tmr;
+	static bu::Timer optimization_tmr;
+	double period;
 
 	// Check if there is at least one application to synchronize
 	if ((!am.HasApplications(Application::READY)) &&
@@ -110,6 +181,10 @@ void ResourceManager::Optimize() {
 	ra.PrintStatusReport();
 	logger->Info("Running Optimization...");
 
+	// Account for a new schedule activation
+	RM_COUNT_EVENT(metrics, RM_SCHED_TOTAL);
+	RM_GET_PERIOD(metrics, RM_SCHED_PERIOD, period);
+
 	//--- Scheduling
 	logger->Notice("====================[ SCHEDULE START ]====================");
 	optimization_tmr.start();
@@ -119,9 +194,11 @@ void ResourceManager::Optimize() {
 	case SchedulerManager::MISSING_POLICY:
 	case SchedulerManager::FAILED:
 		logger->Error("EXC start FAILED (Error: scheduling policy failed)");
+		RM_COUNT_EVENT(metrics, RM_SCHED_FAILED);
 		return;
 	case SchedulerManager::DELAYED:
 		logger->Error("EXC start DELAYED");
+		RM_COUNT_EVENT(metrics, RM_SCHED_DELAYED);
 		return;
 	default:
 		assert(schedResult == SchedulerManager::DONE);
@@ -134,9 +211,13 @@ void ResourceManager::Optimize() {
 	// Check if there is at least one application to synchronize
 	if (!am.HasApplications(Application::SYNC)) {
 		logger->Debug("NO EXC in SYNC state, synchronization not required");
+		RM_COUNT_EVENT(metrics, RM_SCHED_EMPTY);
 		return;
 	}
 
+	// Account for a new synchronizaiton activation
+	RM_COUNT_EVENT(metrics, RM_SYNCH_TOTAL);
+	RM_GET_PERIOD(metrics, RM_SYNCH_PERIOD, period);
 	//--- Synchroniztion
 	logger->Notice("====================[ SYNC START ]====================");
 	optimization_tmr.start();
@@ -153,14 +234,23 @@ void ResourceManager::EvtExcStart() {
 
 	logger->Info("EXC Enabled");
 
+	// Reset timer for START event execution time collection
+	RM_RESET_TIMING(rm_tmr);
+
 	// TODO add here a suitable policy to trigger the optimization
 
 	Optimize();
+	
+	// Collecing execution metrics
+	RM_GET_TIMING(metrics, RM_EVT_TIME_START, rm_tmr);
 }
 
 void ResourceManager::EvtExcStop() {
 
 	logger->Info("EXC Disabled");
+
+	// Reset timer for START event execution time collection
+	RM_RESET_TIMING(rm_tmr);
 
 	// TODO add here a suitable policy to trigger the optimization
 	
@@ -170,9 +260,16 @@ void ResourceManager::EvtExcStop() {
 	::usleep(500000);
 
 	Optimize();
+
+	// Collecing execution metrics
+	RM_GET_TIMING(metrics, RM_EVT_TIME_STOP, rm_tmr);
 }
 
 void ResourceManager::EvtBbqUsr1() {
+
+	// Reset timer for START event execution time collection
+	RM_RESET_TIMING(rm_tmr);
+
 	logger->Info("");
 	logger->Info("==========[ Status Queues ]============"
 			"========================================");
@@ -203,14 +300,24 @@ void ResourceManager::EvtBbqUsr1() {
 	// Clear the corresponding event flag
 	logger->Notice("");
 	pendingEvts.reset(BBQ_USR1);
+
+	// Collecing execution metrics
+	RM_GET_TIMING(metrics, RM_EVT_TIME_USR1, rm_tmr);
 }
 
 void ResourceManager::EvtBbqUsr2() {
+
+	// Reset timer for START event execution time collection
+	RM_RESET_TIMING(rm_tmr);
+
 	logger->Debug("Dumping metrics collection...");
 	mc.DumpMetrics();
 
 	// Clear the corresponding event flag
 	pendingEvts.reset(BBQ_USR2);
+
+	// Collecing execution metrics
+	RM_GET_TIMING(metrics, RM_EVT_TIME_USR2, rm_tmr);
 }
 
 void ResourceManager::EvtBbqExit() {
@@ -233,6 +340,7 @@ void ResourceManager::EvtBbqExit() {
 
 void ResourceManager::ControlLoop() {
 	std::unique_lock<std::mutex> pendingEvts_ul(pendingEvts_mtx);
+	double period;
 
 	// Wait for a new event
 	if (!pendingEvts.any())
@@ -248,22 +356,34 @@ void ResourceManager::ControlLoop() {
 		if (!pendingEvts[evt-1])
 			continue;
 
+		// Account for a new event
+		RM_COUNT_EVENT(metrics, RM_EVT_TOTAL);
+		RM_GET_PERIOD(metrics, RM_EVT_PERIOD, period);
+
 		// Dispatching events to handlers
 		switch(evt-1) {
 		case EXC_START:
 			logger->Debug("Event [EXC_START]");
 			EvtExcStart();
+			RM_COUNT_EVENT(metrics, RM_EVT_START);
+			RM_GET_PERIOD(metrics, RM_EVT_PERIOD_START, period);
 			break;
 		case EXC_STOP:
 			logger->Debug("Event [EXC_STOP]");
 			EvtExcStop();
+			RM_COUNT_EVENT(metrics, RM_EVT_STOP);
+			RM_GET_PERIOD(metrics, RM_EVT_PERIOD_STOP, period);
 			break;
 		case BBQ_USR1:
 			logger->Debug("Event [BBQ_USR1]");
+			RM_COUNT_EVENT(metrics, RM_EVT_USR1);
+			RM_GET_PERIOD(metrics, RM_EVT_PERIOD_USR1, period);
 			EvtBbqUsr1();
 			return;
 		case BBQ_USR2:
 			logger->Debug("Event [BBQ_USR2]");
+			RM_COUNT_EVENT(metrics, RM_EVT_USR2);
+			RM_GET_PERIOD(metrics, RM_EVT_PERIOD_USR2, period);
 			EvtBbqUsr2();
 			return;
 		case BBQ_EXIT:
