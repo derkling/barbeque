b2ab543ee438c9c9302e1266ebbd5b7380a6a5f4
Giuseppe Massari <joe.massanga@gmail.com>
2011-09-10 11:11:11 +0200

    [WorkingMode] Extended MAX_NUM_CLUSTERS
    Value changed from 6 to 16.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- include/bbque/app/working_mode_status.h
+++ include/bbque/app/working_mode_status.h
@@ -27,7 +27,7 @@
 #include "bbque/app/plugin_data.h"
 #include "bbque/res/resource_accounter.h"
 
-#define MAX_NUM_CLUSTERS 6
+#define MAX_NUM_CLUSTERS 16
 
 using bbque::res::ResID_t;
 using bbque::res::ResourceUsage;
