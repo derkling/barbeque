ca90502627bfff2f7e44a067036f8b18a2407b47
Giuseppe Massari <joe.massanga@gmail.com>
2011-10-19 11:29:03 +0200

    [Recipe] Log level changed in AddConstraint() msg
    The log messages have been switched from Info to Debug.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- bbque/app/recipe.cc
+++ bbque/app/recipe.cc
@@ -87,7 +87,7 @@ void Recipe::AddConstraint(std::string const & rsrc_path,
 	if (cons_it != constraints.end()) {
 		(cons_it->second)->lower = std::max((cons_it->second)->lower, lb);
 		(cons_it->second)->upper = std::max((cons_it->second)->upper, ub);
-		logger->Info("Constraint (edit): %s L=%d U=%d", rsrc_path.c_str(),
+		logger->Debug("Constraint (edit): %s L=%d U=%d", rsrc_path.c_str(),
 						(cons_it->second)->lower,
 						(cons_it->second)->upper);
 		return;
@@ -96,7 +96,7 @@ void Recipe::AddConstraint(std::string const & rsrc_path,
 	// Insert a new constraint
 	constraints.insert(std::pair<std::string, ConstrPtr_t>(rsrc_path,
 							ConstrPtr_t(new ResourceConstraint(lb, ub))));
-	logger->Info("Constraint (new): %s L=%llu U=%llu", rsrc_path.c_str(),
+	logger->Debug("Constraint (new): %s L=%llu U=%llu", rsrc_path.c_str(),
 					constraints[rsrc_path]->lower,
 					constraints[rsrc_path]->upper);
 }
