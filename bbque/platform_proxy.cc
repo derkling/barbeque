79b5779b0bde1049d114e4fc03d9c2f9d20a8844
Patrick Bellasi <derkling@gmail.com>
2011-12-05 20:12:04 +0100

    [PP] Add first implementation of a PlatformProxy module
    This is the first instance of a PlatformProxy, which is the Barbeque core
    module in charge to interface the RTRM with the specific target platform.
    The initial implementation, still to be completed, defines the basic services
    exposed to other modules, which allows to:
    1. query for the resources available on the target platform
    2. assign resources to an application
    3. reclaim resources from an application
    
    The PlatformProxy module will provide also a proper platform monitoring service
    which could be used to trigger a RTRM action once resources availability
    should change at run-time.
    
    The PlatformPoxy module is meant to be extended by a platform specific instance,
    to be implemented as a derived class extending from this basic module which
    instead is used to provide the platform independent code to access a generic
    target platform.
    
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

---
+++ bbque/platform_proxy.cc
@@ -0,0 +1,168 @@
+/**
+ *       @file  platform_proxy.cc
+ *      @brief  A proxy to interact with the target platform
+ *
+ * Implementation of the PlatformProxy class
+ *
+ *     @author  Patrick Bellasi (derkling), derkling@google.com
+ *
+ *   @internal
+ *     Created  11/23/2011
+ *    Revision  $Id: doxygen.templates,v 1.3 2010/07/06 09:20:12 mehner Exp $
+ *    Compiler  gcc/g++
+ *     Company  Politecnico di Milano
+ *   Copyright  Copyright (c) 2011, Patrick Bellasi
+ *
+ * This source code is released for free distribution under the terms of the
+ * GNU General Public License as published by the Free Software Foundation.
+ * =============================================================================
+ */
+
+#include "bbque/platform_proxy.h"
+
+#include "bbque/utils/utility.h"
+#include "bbque/modules_factory.h"
+#include "bbque/test_platform_data.h"
+
+#if defined BBQUE_PLATFORM_LINUX
+# include "bbque/pp/linux.h"
+# define  PLATFORM_PROXY LinuxPP
+#elif defined BBQUE_PLATFORM_P2012
+# include "bbque/pp/p2012.h"
+# define  PLATFORM_PROXY P2012PP
+#endif
+
+namespace bb = bbque;
+namespace br = bbque::res;
+
+namespace bbque {
+
+PlatformProxy::PlatformProxy() :
+	trdRunning(false),
+	done(false) {
+
+	// Get a logger module
+	std::string logger_name(PLATFORM_PROXY_NAMESPACE);
+	plugins::LoggerIF::Configuration conf(logger_name.c_str());
+	logger = ModulesFactory::GetLoggerModule(std::cref(conf));
+
+	// Spawn the platform monitoring thread
+	monitor_thd = std::thread(&PlatformProxy::Monitor, this);
+
+}
+
+PlatformProxy::~PlatformProxy() {
+	// Stopping the monitor thread
+	Stop();
+}
+
+PlatformProxy & PlatformProxy::GetInstance() {
+	static PLATFORM_PROXY instance;
+	return instance;
+}
+
+
+void PlatformProxy::Start() {
+	std::unique_lock<std::mutex> ul(trdStatus_mtx);
+
+	logger->Debug("PLAT PRX: starting the monitoring service...");
+	trdRunning = true;
+	trdStatus_cv.notify_one();
+}
+
+void PlatformProxy::Stop() {
+	std::unique_lock<std::mutex> ul(trdStatus_mtx);
+
+	if (done == true)
+		return;
+
+	logger->Debug("PLAT PRX: stopping the monitoring service...");
+	done = true;
+	trdStatus_cv.notify_one();
+}
+
+void PlatformProxy::Monitor() {
+	std::unique_lock<std::mutex> trdStatus_ul(trdStatus_mtx);
+
+	// Set the module name
+	if (prctl(PR_SET_NAME, BBQUE_MODULE_NAME("pp"), 0, 0, 0) != 0) {
+		logger->Error("Set name FAILED! (Error: %s)\n", strerror(errno));
+	}
+
+	// Waiting for thread authorization to start
+	if (!trdRunning)
+		trdStatus_cv.wait(trdStatus_ul);
+
+	trdStatus_ul.unlock();
+
+	logger->Info("PLAT PRX: Monitoring thread STARTED");
+
+	while (!done) {
+		// TODO place here the code to monitor for resources availability and
+		// run-time status (e.g. thermal, load-average)
+		trdStatus_ul.lock();
+		trdStatus_cv.wait(trdStatus_ul);
+	}
+
+	logger->Info("PLAT PRX: Monitoring thread ENDED");
+}
+
+
+PlatformProxy::ExitCode_t
+PlatformProxy::LoadPlatformData() {
+
+#if BBQUE_TEST_PLATFORM_DATA
+	//---------- Loading TEST platform data
+	// This is just a temporary placeholder for the (still-missing)
+	// ResourceAbstraction module
+	bb::TestPlatformData &tpd(bb::TestPlatformData::GetInstance());
+	tpd.LoadPlatformData();
+	return OK;
+#endif // BBQUE_TEST_PLATFORM_DATA
+
+	// TODO place here the code to parse the CGroup configuration and get
+	// the set of Barbeuqe managed resources
+}
+
+PlatformProxy::ExitCode_t
+PlatformProxy::Setup(AppPtr_t papp) {
+	ExitCode_t result = OK;
+
+	logger->Debug("PLAT PRX: platform setup for run-time control "
+			"of app [%s]", papp->StrId());
+	result = _Setup(papp);
+	return result;
+}
+
+PlatformProxy::ExitCode_t
+PlatformProxy::Release(AppPtr_t papp) {
+	ExitCode_t result = OK;
+
+	logger->Debug("PLAT PRX: releasing platform-specific run-time control "
+			"for app [%s]", papp->StrId());
+	result = _Release(papp);
+	return result;
+}
+
+PlatformProxy::ExitCode_t
+PlatformProxy::ReclaimResources(AppPtr_t papp) {
+	ExitCode_t result = OK;
+
+	logger->Debug("PLAT PRX: Reclaiming resources of app [%s]", papp->StrId());
+	result = _ReclaimResources(papp);
+	return result;
+}
+
+PlatformProxy::ExitCode_t
+PlatformProxy::MapResources(AppPtr_t papp, UsagesMapPtr_t pres, bool excl) {
+	br::ResourceAccounter &ra = br::ResourceAccounter::GetInstance();
+	RViewToken_t rvt = ra.GetScheduledView();
+	ExitCode_t result = OK;
+
+	logger->Debug("PLAT PRX: Mapping resources for app [%s], using view [%d]",
+			papp->StrId(), rvt);
+	result = _MapResources(papp, pres, rvt, excl);
+	return result;
+}
+
+} /* bbque */
---
+++ include/bbque/platform_proxy.h
@@ -0,0 +1,265 @@
+/**
+ *       @file  platform_proxy.h
+ *      @brief  A proxy to interact with the target platform
+ *
+ * Definition of the class used by Barbeque RTRM core modules to communicate
+ * with the platform by means of a platforms-specific abstraction and
+ * integration layer. This class provides all the basic services which are
+ * required by the core modules in order to:
+ * <li><i>get a platform description:</i> by collecting a description of
+ * resources availability and their run-time sate</li>
+ * <li><i>control resource partitioning:</i> by binding resources to the
+ * assignee applications</li>
+ *
+ *     @author  Patrick Bellasi (derkling), derkling@google.com
+ *
+ *   @internal
+ *     Created  11/23/2011
+ *    Revision  $Id: doxygen.templates,v 1.3 2010/07/06 09:20:12 mehner Exp $
+ *    Compiler  gcc/g++
+ *     Company  Politecnico di Milano
+ *   Copyright  Copyright (c) 2011, Patrick Bellasi
+ *
+ * This source code is released for free distribution under the terms of the
+ * GNU General Public License as published by the Free Software Foundation.
+ * =============================================================================
+ */
+
+#ifndef BBQUE_PLATFORM_PROXY_H_
+#define BBQUE_PLATFORM_PROXY_H_
+
+#include "bbque/plugins/logger.h"
+#include "bbque/app/application.h"
+#include "bbque/res/resource_accounter.h"
+
+#include <memory>
+#include <thread>
+
+#define PLATFORM_PROXY_NAMESPACE "bq.pp"
+
+using bbque::app::AppPtr_t;
+using bbque::res::RViewToken_t;
+using bbque::res::UsagesMapPtr_t;
+
+namespace bbque {
+
+/**
+ * @brief The Platform Proxy module
+ */
+class PlatformProxy {
+
+public:
+
+	/**
+	 * @brief Exit codes returned by methods of this class
+	 */
+	typedef enum ExitCode {
+		OK = 0,
+		PLATFORM_DATA_NOT_FOUND,
+		PLATFORM_DATA_PARSING_ERROR,
+		MAPPING_FAILED
+	} ExitCode_t;
+
+/**
+ * @defgroup group_plt_prx Platform Proxy
+ * @{
+ *
+ * @name Basic Infrastructure and Initialization
+ * @{
+ * The <tt>PlatformProxy</tt> is the BarbequeRTRM core module in charge to
+ * manage the communication with the target platform by means of a
+ * platforms-specific abstraction and integration layer.
+ *
+ * This class provides all the basic services which are required by the core
+ * modules in order to:
+ * <ul>
+ * <li><i>get a platform description:</i> by collecting a description of
+ * resources availability and their run-time sate</li>
+ * <li><i>control resource partitioning:</i> by binding resources to the
+ * assignee applications</li>
+ * </ul>
+ */
+
+	/**
+	 * @brief Get a reference to the paltform proxy
+	 */
+	static PlatformProxy & GetInstance();
+
+	/**
+	 * @brief Release all the platform proxy resources
+	 */
+	~PlatformProxy();
+
+	/**
+	 * @brief Start the platform monitoring thread
+	 *
+	 * The platform integration layer provides support for monitoring
+	 * resources status at run-time. This methods setup and trigger the
+	 * execution of the proper platform monitoring thread.
+	 */
+	void Start();
+
+	/**
+	 * @brief Stop the platform monitoring thread
+	 *
+	 * The platform integration layer provides support for monitoring
+	 * resources status at run-time. This methods setup and trigger the
+	 * execution of the proper platform monitoring thread.
+	 */
+	void Stop();
+
+/**
+ * @}
+ * @name Platform description
+ * @{
+ */
+
+	/**
+	 * @brief Look-up for a platform description and load it.
+	 *
+	 * A description of all platform resources is looked-up by a call of this
+	 * method which provides also their registration into the ResourceManager
+	 * module.
+	 */
+	ExitCode_t LoadPlatformData();
+
+/**
+ * @}
+ * @name Resource state monitoring
+ * @{
+ */
+
+
+/**
+ * @}
+ * @name Resource binding
+ * @{
+ */
+
+	/**
+	 * @brief Setup platform data required to manage the specified applciation
+	 *
+	 * Once a new application enter the system, a set of platform specific
+	 * applications data could be require in order to properly set-up the
+	 * platform for run-time management. This method should be called to
+	 * perpare the groud for run-time platform control.
+	 */
+	ExitCode_t Setup(AppPtr_t papp);
+
+
+	/**
+	 * @brief Release platform data used to manage the specified application
+	 *
+	 * Once an application exit the system, this method should be called to
+	 * properly release all the platform specific data used to manage it at
+	 * run-time.
+	 */
+	ExitCode_t Release(AppPtr_t papp);
+
+	/**
+	 * @brief Release all the resources assigned to the specified application.
+	 *
+	 * @param papp The application which resources are reclaimed
+	 */
+	ExitCode_t ReclaimResources(AppPtr_t papp);
+
+	/**
+	 * @brief Bind the specified resources to the specified application.
+	 *
+	 * @param papp The application which resources are assigned
+	 * @param pres The resources to be assigned
+	 * @param excl If true the specified resources are assigned for exclusive
+	 * usage to the application
+	 */
+	ExitCode_t MapResources(AppPtr_t papp, UsagesMapPtr_t pres,
+			bool excl = true);
+
+/**
+ * @}
+ * @}
+ */
+
+private:
+
+
+	/**
+	 * @brief The platform monitoring thread
+	 */
+	std::thread monitor_thd;
+
+	/**
+	 * @brief Status of the monitoring thread
+	 *
+	 * This variable is set true when the monitoring thread has been autorized
+	 * to run.
+	 */
+	bool trdRunning;
+
+
+	/**
+	 * @brief Set true to terminate the moniroting thread
+	 *
+	 * This is set to true to end the monitoring thread.
+	 */
+	bool done;
+
+	/**
+	 * @brief Mutex controlling the thread execution
+	 */
+	std::mutex trdStatus_mtx;
+
+	/**
+	 * @brief Conditional variable used to signal the monitoring thread
+	 */
+	std::condition_variable trdStatus_cv;
+
+	/**
+	 * @brief The platform monitoring thread.
+	 */
+	void Monitor();
+
+
+protected:
+
+	/**
+	 * @brief The logger module
+	 */
+	plugins::LoggerIF *logger;
+
+	/**
+	 * @biref Build a new platform proxy
+	 */
+	PlatformProxy();
+
+/*******************************************************************************
+ *  Platform Specific (low-level) methods
+ ******************************************************************************/
+
+	/**
+	 * @brief Platform specific resource setup interface.
+	 */
+	virtual ExitCode_t _Setup(AppPtr_t papp) = 0;
+
+	/**
+	 * @brief Platform specific resources release interface.
+	 */
+	virtual ExitCode_t _Release(AppPtr_t papp) = 0;
+
+	/**
+	 * @brief Platform specific resource claiming interface.
+	 */
+	virtual ExitCode_t _ReclaimResources(AppPtr_t papp) = 0;
+
+
+	/**
+	 * @brief Platform specifi resource binding interface.
+	 */
+	virtual ExitCode_t _MapResources(AppPtr_t papp, UsagesMapPtr_t pres,
+			RViewToken_t rvt, bool excl) = 0;
+
+
+};
+
+} /* bbque */
+
+#endif /* end of include guard: BBQUE_PLATFORM_PROXY_H_ */
