880b5a158645580832c617edca1575ed75307e10
Giuseppe Massari <joe.massanga@gmail.com>
2011-12-13 11:35:29 +0100

    [TEST] Scenario1 updated
    The script test "scenario1.sh" has been updated according to the last
    command-line options of BbqueRTLibTestApp.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- testing/scenario1.sh
+++ testing/scenario1.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# This should generate a MIGREC case
-
-./build/Debug/testing/rtlib/rtlib_example01 r_02 12 &
+./out/usr/bin/BbqRTLibTestApp -r r_02 -w 12 -c 120 &
 sleep 1
-./build/Debug/testing/rtlib/rtlib_example01 r_03 10 &
+./out/usr/bin/BbqRTLibTestApp -r r_03 -w 10 -c 100 &
 sleep 1
-./build/Debug/testing/rtlib/rtlib_example01 r_05 10 &
+./out/usr/bin/BbqRTLibTestApp -r r_05 -w 10 -c 100 &
 sleep 2
-./build/Debug/testing/rtlib/rtlib_example01 r_01 4 
+./out/usr/bin/BbqRTLibTestApp -r r_01 -w 4 -c 40 &
 
