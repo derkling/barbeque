8071313f26f7dccc8f9af6899898714d60e25faf
Giuseppe Massari <joe.massanga@gmail.com>
2012-01-22 10:31:37 +0100

    [RA] Remove methods exported in ConfIF
    Methods RegisterResource(), BookResources() and ReleaseResources() were
    erroneously exported into the "configure" IF. This has been fixed since
    these methods should be called by core components only.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- include/bbque/res/resource_accounter.h
+++ include/bbque/res/resource_accounter.h
@@ -196,19 +196,65 @@ public:
 	void PrintStatusReport(RViewToken_t vtok = 0, bool verbose = false) const;
 
 	/**
-	 * @see ResourceAccounterConfIF
+	 * @brief Register a resource
+	 *
+	 * Setup informations about a resource installed into the system.
+	 * Resources can be system-wide or placed on the platform. A resource is
+	 * identified by its pathname. The latters for instance must have the
+	 * "arch." suffix in front. While the formers doesn't show any suffix.
+	 *
+	 * Thus we could have resource paths as below :
+	 *
+	 * "mem0" 		: system memory
+	 * "arch.mem0 	: internal memory (to the platform)
+	 * "arch.tile.cluster2 		: cluster 2 of the platform
+	 * "arch.tile.cluster0.pe1 	: processing element 1 in cluster 0
+	 *
+	 * @param path Resource path
+	 * @param units Units for the amount value (i.e. "1", "Kbps", "Mb", ...)
+	 * @param amount The total amount available
+	 *
+	 * @return RA_SUCCESS if the resource has been successfully registered.
+	 * RA_ERR_MISS_PATH if the path string is empty. RA_ERR_MEM if the
+	 * resource descriptor cannot be allocated.
 	 */
 	ExitCode_t RegisterResource(std::string const & path,
 			std::string const & units, uint64_t amount);
 
 	/**
-	 * @see ResourceAccounterConfIF
+	 * @brief Book e a set of resources
+	 *
+	 * The method first check that the application doesn't hold another
+	 * resource set, then check thier availability, and finally reserves, for
+	 * each resource in the usages map specified, the required quantity.
+	 *
+	 * @param app The application requiring resource usages
+	 * @param usages Map of ResourceUsage objects
+	 * @param vtok The token referencing the resource state view
+	 * @param do_check If true the controls upon set validity and resources
+	 * availability are enabled
+	 *
+	 * @return RA_SUCCESS if the operation has been successfully performed.
+	 * RA_ERR_MISS_APP if the application descriptor is null.
+	 * RA_ERR_MISS_USAGES if the resource usages map is empty.
+	 * RA_ERR_MISS_VIEW if the resource state view referenced by the given
+	 * token cannot be retrieved.
+	 * RA_ERR_USAGE_EXC if the resource set required is not completely
+	 * available.
 	 */
 	ExitCode_t BookResources(AppPtr_t papp, UsagesMapPtr_t const & usages,
 			RViewToken_t vtok = 0, bool do_check = true);
 
 	/**
-	 * @see ResourceAccounterConfIF
+	 * @brief Release the resources
+	 *
+	 * The method is typically called when an application stops running
+	 * (exit or is killed) and releases all the resource usages.
+	 * It lookups the current set of resource usages of the application and
+	 * release it all.
+	 *
+	 * @param app The application holding the resources
+	 * @param vtok The token referencing the resource state view
 	 */
 	void ReleaseResources(AppPtr_t papp, RViewToken_t vtok = 0);
 
--- include/bbque/res/resource_accounter_conf.h
+++ include/bbque/res/resource_accounter_conf.h
@@ -46,63 +46,6 @@ class ResourceAccounterConfIF: public ResourceAccounterStatusIF {
 public:
 
 	/**
-
-	/**
-	 * @brief Register a resource
-	 *
-	 * Setup informations about a resource installed into the system.
-	 * Resources can be system-wide or placed on the platform. A resource is
-	 * identified by its pathname. The latters for instance must have the
-	 * "arch." suffix in front. While the formers doesn't show any suffix.
-	 *
-	 * Thus we could have resource paths as below :
-	 *
-	 * "mem0" 		: system memory
-	 * "spi0" 		: SPI system bus
-	 * "arch.mem0 	: internal memory (to the platform)
-	 * "arch.tile.cluster2 		: cluster 2 of the platform
-	 * "arch.tile.cluster0.pe1 	: processing element 1 in cluster 0
-	 *
-	 * @param path Resource path
-	 * @param units Units for the amount value (i.e. "1", "Kbps", "Mb", ...)
-	 * @param amount The total amount available
-	 * @return An exit code (@see ExitCode_t)
-	 */
-	virtual ExitCode_t RegisterResource(std::string const & path,
-			std::string const & units, uint64_t amount) = 0;
-
-	/**
-	 * @brief Book e a set of resources
-	 *
-	 * The method first check that the application doesn't hold another
-	 * resource set, then check thier availability, and finally reserves, for
-	 * each resource in the usages map specified, the required quantity.
-	 *
-	 * @param app The application requiring resource usages
-	 * @param usages Map of ResourceUsage objects
-	 * @param vtok The token referencing the resource state view
-	 * @param do_check If true the controls upon set validity and resources
-	 * availability are enabled
-	 * @return An exit code (@see ExitCode_t)
-	 */
-	virtual ExitCode_t BookResources(AppPtr_t app,
-			UsagesMapPtr_t const & usages, RViewToken_t vtok = 0,
-			bool do_check = true) = 0;
-
-	/**
-	 * @brief Release the resources
-	 *
-	 * The method is typically called when an application stops running
-	 * (exit or is killed) and releases all the resource usages.
-	 * It lookups the current set of resource usages of the application and
-	 * release it all.
-	 *
-	 * @param app The application holding the resources
-	 * @param vtok The token referencing the resource state view
-	 */
-	virtual void ReleaseResources(AppPtr_t app, RViewToken_t vtok = 0) = 0;
-
-	/**
 	 * @brief Get a new resources view
 	 *
 	 * A component (core or module) can require a "personal" view of the
