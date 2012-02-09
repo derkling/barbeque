5b553d97c328866aa4b9f7b620c0d65cd021df26
Giuseppe Massari <joe.massanga@gmail.com>
2012-02-09 17:15:48 +0100

    [SV] AppPtr_t usage replaced by AppCPtr_t
    For security purpose the methods using arguments of type AppPtr_t have been
    modified by using the more restricted type AppCPtr_t.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- include/bbque/system_view.h
+++ include/bbque/system_view.h
@@ -24,6 +24,7 @@
 using bbque::ApplicationManager;
 using bbque::ApplicationManagerStatusIF;
 using bbque::app::ApplicationStatusIF;
+using bbque::app::AppCPtr_t;
 using bbque::res::ResourceAccounter;
 using bbque::res::ResourceAccounterStatusIF;
 using bbque::res::ResourcePtr_t;
@@ -58,14 +59,14 @@ public:
 	/**
 	 * @brief Return the first app at the specified priority
 	 */
-	inline AppPtr_t GetFirstWithPrio(AppPrio_t prio, AppsUidMapIt & ait) {
+	inline AppCPtr_t GetFirstWithPrio(AppPrio_t prio, AppsUidMapIt & ait) {
 		return am.GetFirst(prio, ait);
 	}
 
 	/**
 	 * @brief Return the next app at the specified priority
 	 */
-	inline AppPtr_t GetNextWithPrio(AppPrio_t prio, AppsUidMapIt & ait) {
+	inline AppCPtr_t GetNextWithPrio(AppPrio_t prio, AppsUidMapIt & ait) {
 		return am.GetNext(prio, ait);
 	}
 
@@ -73,22 +74,22 @@ public:
 	/**
 	 * @brief Return the map containing all the ready applications
 	 */
-	inline AppPtr_t GetFirstReady(AppsUidMapIt & ait) {
+	inline AppCPtr_t GetFirstReady(AppsUidMapIt & ait) {
 		return am.GetFirst(ApplicationStatusIF::READY, ait);
 	}
 
-	inline AppPtr_t GetNextReady(AppsUidMapIt & ait) {
+	inline AppCPtr_t GetNextReady(AppsUidMapIt & ait) {
 		return am.GetNext(ApplicationStatusIF::READY, ait);
 	}
 
 	/**
 	 * @brief Map of running applications (descriptors)
 	 */
-	inline AppPtr_t GetFirstRunning(AppsUidMapIt & ait) {
+	inline AppCPtr_t GetFirstRunning(AppsUidMapIt & ait) {
 		return am.GetFirst(ApplicationStatusIF::RUNNING, ait);
 	}
 
-	inline AppPtr_t GetNextRunning(AppsUidMapIt & ait) {
+	inline AppCPtr_t GetNextRunning(AppsUidMapIt & ait) {
 		return am.GetNext(ApplicationStatusIF::RUNNING, ait);
 	}
 
@@ -96,11 +97,11 @@ public:
 	 * @brief Map of blocked applications (descriptors)
 	 */
 
-	inline AppPtr_t GetFirstBlocked(AppsUidMapIt & ait) {
+	inline AppCPtr_t GetFirstBlocked(AppsUidMapIt & ait) {
 		return am.GetFirst(ApplicationStatusIF::BLOCKED, ait);
 	}
 
-	inline AppPtr_t GetNextBlocked(AppsUidMapIt & ait) {
+	inline AppCPtr_t GetNextBlocked(AppsUidMapIt & ait) {
 		return am.GetNext(ApplicationStatusIF::BLOCKED, ait);
 	}
 
@@ -139,7 +140,7 @@ public:
 	 * @see ResourceAccounterStatusIF::Available()
 	 */
 	inline uint64_t ResourceAvailable(std::string const & path,
-			RViewToken_t vtok = 0, AppPtr_t papp = AppPtr_t()) const {
+			RViewToken_t vtok = 0, AppCPtr_t papp = AppCPtr_t()) const {
 		return ra.Available(path, vtok, papp);
 	}
 
@@ -147,7 +148,7 @@ public:
 	 * @see ResourceAccounterStatusIF::Available()
 	 */
 	inline uint64_t ResourceAvailable(ResourcePtrList_t & rsrc_list,
-			RViewToken_t vtok = 0, AppPtr_t papp = AppPtr_t()) const {
+			RViewToken_t vtok = 0, AppCPtr_t papp = AppCPtr_t()) const {
 		return ra.Available(rsrc_list, vtok, papp);
 	}
 
