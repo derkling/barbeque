8c647ac5e5650f0e31ff3cda0dcd147b1958fe34
Giuseppe Massari <joe.massanga@gmail.com>
2012-02-13 18:04:21 +0100

    [MCT_Value] AWM value metrics contribute
    This a dummy metrics contribute: The AWM value. No real computations are
    performed, but a simple call to the get method of AWM is done.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

---
+++ plugins/schedpol/contrib/mct_value.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2012  Politecnico di Milano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BBQUE_MCT_VALUE_
+#define BBQUE_MCT_VALUE_
+
+#include "metrics_contribute.h"
+
+
+namespace bbque { namespace plugins {
+
+
+class MCTValue: public MetricsContribute {
+
+public:
+
+	/**
+	 * @brief Constructor
+	 */
+	MCTValue(const char * _name, uint16_t const cfg_params[]):
+		MetricsContribute(_name, cfg_params) {
+	};
+
+private:
+
+	/**
+	 * @brief Compute the AWM value contribute
+	 */
+	ExitCode_t _Compute(EvalEntity_t const & evl_ent, float & ctrib);
+
+};
+
+} // plugins
+
+} // bbque
+
+#endif // BBQUE_MCT_VALUE_
+
+
