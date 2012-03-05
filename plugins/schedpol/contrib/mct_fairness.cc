5ef6bdbe00e1022598df409c0d46bf574166580c
Giuseppe Massari <joe.massanga@gmail.com>
2012-03-05 16:55:32 +0100

    [MCT_Fairness] Fairness metrics contribute
    Introduced the "fairness" contribute.
    The expected behavior is to return higher contribute indexes for
    scheduling choices that assign a "fair" amount of resource to the
    application/EXC, taking into account the amount of applications to schedule
    (in the same priority class). Choices of assignment that overpass an
    identified quota (i.e. fair partition) are penalized exponentially.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

---
+++ plugins/schedpol/contrib/mct_fairness.cc
@@ -0,0 +1,179 @@
+/*
+ * Copyright (C) 2012  Politecnico di Milano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "mct_fairness.h"
+
+#include <algorithm>
+
+namespace po = boost::program_options;
+
+namespace bbque { namespace plugins {
+
+/** Set default congestion penalties values */
+uint16_t MCTFairness::penalties_default[MCT_RSRC_COUNT] = {
+	5,
+	5
+};
+
+MCTFairness::MCTFairness(const char * _name, uint16_t const cfg_params[]):
+	MetricsContribute(_name, cfg_params) {
+	char conf_str[50];
+
+	// Configuration parameters
+	po::options_description opts_desc("Fairness contribute parameters");
+
+	// Base for exponential
+	snprintf(conf_str, 50, MCT_CONF_BASE_STR"%s.expbase", name);
+	opts_desc.add_options()
+		(conf_str,
+		 po::value<uint16_t>(&expbase)->default_value(DEFAULT_CONG_EXPBASE),
+		 "Base for exponential function");
+		;
+
+	// Congestion penalties
+	for (int i = 0; i < MCT_RSRC_COUNT; ++i) {
+		snprintf(conf_str, 50, MCT_CONF_BASE_STR"%s.penalty.%s",
+				name, ResourceNames[i]);
+
+		logger->Debug("%s", conf_str);
+		opts_desc.add_options()
+			(conf_str,
+			 po::value<uint16_t>
+				(&penalties_int[i])->default_value(penalties_default[i]),
+			 "Fairness penalty per resource");
+		;
+	}
+
+	po::variables_map opts_vm;
+	cm.ParseConfigurationFile(opts_desc, opts_vm);
+
+	// Boundaries enforcement (0 <= penalty <= 100)
+	for (int i = 0; i < MCT_RSRC_COUNT; ++i) {
+		if (penalties_int[i] > 100) {
+			logger->Warn("Parameter penalty.%s out of range [0,100]: "
+					"found %d. Setting to %d", ResourceNames[i],
+					penalties_int[i], penalties_default[i]);
+			penalties_int[i] = penalties_default[i];
+		}
+		logger->Debug("penalty.%s \t= %.2f", ResourceNames[i],
+				static_cast<float>(penalties_int[i]) / 100.0);
+	}
+}
+
+MetricsContribute::ExitCode_t
+MCTFairness::Init(void * params) {
+	AppPrio_t * prio;
+	prio = static_cast<AppPrio_t *>(params);
+
+	// Applications/EXC to schedule, given the priority level
+	num_apps = sv->ApplicationsCount(*prio);
+	logger->Debug("%d Applications/EXC for priority level %d", num_apps, *prio);
+
+	// Get the total amount of resource per types
+	for (int i = 0; i < MCT_RSRC_COUNT; ++i) {
+		rsrc_avail[i] = sv->ResourceAvailable(ResourceGenPaths[i], vtok);
+		fair_parts[i] = rsrc_avail[i] / num_apps;
+		logger->Debug("R{%s} AVL:%lu Fair partition:%lu",
+				ResourceGenPaths[i], rsrc_avail[i], fair_parts[i]);
+	}
+
+	return MCT_SUCCESS;
+}
+
+MetricsContribute::ExitCode_t
+MCTFairness::_Compute(EvalEntity_t const & evl_ent, float & ctrib) {
+	UsagesMap_t::const_iterator usage_it;
+	CLEParams_t params;
+	float ru_index;
+	float penalty;
+	uint64_t clust_rsrc_avl;
+	uint64_t clust_fract;
+	uint64_t clust_fair_part;
+	ctrib = 1.0;
+
+	// Fixed function parameters
+	params.k = 1.0;
+	params.exp.base = expbase;
+
+	// Iterate the whole set of resource usage
+	for_each_sched_resource_usage(evl_ent, usage_it) {
+		std::string const & rsrc_path(usage_it->first);
+		UsagePtr_t const & pusage(usage_it->second);
+		ResourcePtrList_t & rsrc_bind(pusage->GetBindingList());
+
+		// Resource availability (in the bound cluster)
+		clust_rsrc_avl = sv->ResourceAvailable(rsrc_bind, vtok);
+		logger->Debug("%s: R{%s} resource availability: %lu", evl_ent.StrId(),
+				rsrc_path.c_str(), clust_rsrc_avl);
+
+		// Compute the cluster factor (resource type related)
+		std::string rsrc_name(ResourcePathUtils::GetNameTemplate(rsrc_path));
+		if (rsrc_name.compare(ResourceNames[MCT_RSRC_PE]) == 0) {
+			clust_fract = ceil(static_cast<double>(clust_rsrc_avl) /
+					fair_parts[MCT_RSRC_PE]);
+			penalty = static_cast<float>(penalties_int[MCT_RSRC_PE]) / 100.0;
+		}
+		else {
+			clust_fract = ceil(static_cast<double>(clust_rsrc_avl) /
+					fair_parts[MCT_RSRC_MEM]);
+			penalty = static_cast<float>(penalties_int[MCT_RSRC_MEM]) / 100.0;
+		}
+
+		logger->Debug("%s: R{%s} cluster fraction: %lu", evl_ent.StrId(),
+				rsrc_path.c_str(), clust_fract);
+
+		// Compute the cluster fair partition
+		clust_fair_part = std::min<uint64_t>(clust_rsrc_avl,
+				clust_rsrc_avl / clust_fract);
+		logger->Debug("%s: R{%s} cluster fair partition: %lu",
+				evl_ent.StrId(), rsrc_path.c_str(), clust_fair_part);
+
+		// Set function parameters
+		SetIndexParameters(clust_fair_part, clust_rsrc_avl, penalty, params);
+
+		// Compute the region index
+		//ru_index = CLEIndex(clust_fair_part, clust_fair_part,
+		ru_index = CLEIndex(0, clust_fair_part, pusage->GetAmount(), params);
+		logger->Debug("%s: R{%s} index = %.4f", evl_ent.StrId(),
+				rsrc_path.c_str(), ru_index);
+
+		// Update the contribute if the index is lower, i.e. the most
+		// penalizing request dominates
+		ru_index < ctrib ? ctrib = ru_index: ctrib;
+	}
+
+	return MCT_SUCCESS;
+}
+
+void MCTFairness::SetIndexParameters(uint64_t cfp,
+		uint64_t cra,
+		float & penalty,
+		CLEParams_t & params) {
+	// Linear parameters
+	params.lin.xoffset = 0.0;
+	params.lin.scale = penalty / static_cast<float>(cfp);
+
+	// Exponential parameters
+	params.exp.yscale = (1.0 - penalty) / (params.exp.base - 1.0);
+	params.exp.xscale = static_cast<float>(cfp) - static_cast<float>(cra);
+	params.exp.xoffset = static_cast<float>(cra);
+}
+
+
+} // namespace plugins
+
+} // namespace bbque
---
+++ plugins/schedpol/contrib/mct_fairness.h
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) 2012  Politecnico di Milano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BBQUE_MCT_FAIRNESS_
+#define BBQUE_MCT_FAIRNESS_
+
+#include "metrics_contribute.h"
+
+#define DEFAULT_CONG_EXPBASE 	2
+
+using bbque::app::AppPrio_t;
+
+namespace bbque { namespace plugins {
+
+
+class MCTFairness: public MetricsContribute {
+
+public:
+
+	/**
+	 * @brief Constructor
+	 *
+	 * @param name A name identifying the specific contribute
+	 * @param cfg_params Global configuration parameters
+	 */
+	MCTFairness(const char * _name, uint16_t const cfg_params[]);
+
+	/**
+	 * @brief Perform per-priority class information setup
+	 *
+	 * Get the number of applications in the given priority level, the
+	 * availability (not clustered), and computes the fair partitions of each
+	 * resource type
+	 *
+	 * @param params Expected pointer to AppPrio_t type
+	 *
+	 * @return MCT_SUCCESS. No error conditions expected
+	 */
+	ExitCode_t Init(void * params);
+
+private:
+
+	/** Base for exponential functions used in the computation */
+	uint16_t expbase;
+	/**
+	 * Congestion penalties per resource type. This stores the values parsed
+	 * from the configuration file.
+	 *
+	 * 0 = "pe"
+	 * 1 = "mem"
+	 */
+	uint16_t penalties_int[MCT_RSRC_COUNT];
+
+	/** Default values for the congestion penalties */
+	static uint16_t penalties_default[MCT_RSRC_COUNT];
+
+
+	/** Number of applications to schedule */
+	uint16_t num_apps;
+
+	/** Resource availability */
+	uint64_t rsrc_avail[MCT_RSRC_COUNT];
+
+	/** Fair partitions */
+	uint64_t fair_parts[MCT_RSRC_COUNT];
+
+
+	/**
+	 * @brief Compute the congestion contribute
+	 *
+	 * @param evl_ent The entity to evaluate (EXC/AWM/ClusterID)
+	 * @param ctrib The contribute to set
+	 *
+	 * @return MCT_SUCCESS for success
+	 */
+	ExitCode_t _Compute(EvalEntity_t const & evl_ent, float & ctrib);
+
+	/**
+	 * @brief Set the parameters for the filter function
+	 *
+	 * More in detail the parameters set are exclusively the ones of the
+	 * exponential function, since the Sub Fair Region (SFR) returns the
+	 * constant index (1) and the Over Fair Region (OFR) the result of the
+	 * exponential function.
+	 *
+	 * @param cfp Cluster fair partition. Used to compute the "x-scale"
+	 * parameter.
+	 * @param cra Cluster resource availability. This is the "x-offset" and a
+	 * component of the "x-scale" parameter.
+	 * @param penalty The un-fairness penalty of the resource
+	 * @param params Parameters structure to fill
+	 */
+	void SetIndexParameters(uint64_t cfp, uint64_t cra,	float & penalty,
+			CLEParams_t & params);
+
+};
+
+} // plugins
+
+} // bbque
+
+#endif // BBQUE_MCT_FAIRNESS_
