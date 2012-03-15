fba2be16073085e5320e09e20a4cfe5dcb312ef7
Giuseppe Massari <joe.massanga@gmail.com>
2012-03-15 17:21:29 +0100

    [MCT_Value] Partitioned index
    The contribute is now computed as follows:
    
    	index = w1 * AWM_Value + w2 * NAP
    
    where w1 + w2 = 1.
    
    This means that it is partitioned among AWM static value and NAP
    percentage set by the application.
    
    Signed-off-by: Giuseppe Massari <joe.massanga@gmail.com>

--- plugins/schedpol/contrib/mct_value.cc
+++ plugins/schedpol/contrib/mct_value.cc
@@ -78,79 +78,25 @@ MCTValue::_Compute(EvalEntity_t const & evl_ent, float & ctrib) {
 	std::string rsrc_tmp_path;
 	UsagesMap_t::const_iterator usage_it;
 	AwmPtr_t const & curr_awm(evl_ent.papp->CurrentAWM());
-	//uint8_t ggap = 0;
-	float ggap = 0;
-	uint64_t curr_awm_rsrc_usage;
-	float target_usage = 0.0;
-	float penalty = 0.0;
-	float index = 1.0;
+	float nap = 0;
 
-	// Pre-set the index contribute to the AWM static value
+	// Initialize the index contribute to the AWM static value
 	ctrib = 0.4 * evl_ent.pawm->Value();
-	logger->Notice("####### %s: AWM Value part: %.4f", evl_ent.StrId(), ctrib);
+	logger->Debug("%s: AWM static value: %.4f", evl_ent.StrId(), ctrib);
 
-	ggap = 0.6 * static_cast<float>(evl_ent.papp->GetGoalGap()) / 100.0;
-
-
-	// Goal-Gap set?
-	//ggap = evl_ent.papp->GetGoalGap();
-	//logger->Debug("%s: Goal Gap: %d", evl_ent.StrId(), ggap);
-	if (!curr_awm || (ggap == 0) ||
+	// NAP set?
+	nap = 0.6 * static_cast<float>(evl_ent.papp->GetGoalGap()) / 100.0;
+	if (!curr_awm || (nap == 0) ||
 			(curr_awm->Value() >= evl_ent.pawm->Value()))
 		return MCT_SUCCESS;
 
-	logger->Notice("####### %s: AWM Goal Gap (Nap = %d/100): %.4f", evl_ent.StrId(),
-			evl_ent.papp->GetGoalGap(), ggap);
-	ctrib += ggap;
+	logger->Debug("%s: Normalized Actual Penalty (NAP) = %d/100): %.4f",
+			evl_ent.StrId(), evl_ent.papp->GetGoalGap(), nap);
 
-#if 0	
+	// Add the NAP part to the contribute
+	ctrib += nap;
 
-	// Resource usages of the current entity (AWM + Cluster)
-	for_each_sched_resource_usage(evl_ent, usage_it) {
-		std::string const & rsrc_path(usage_it->first);
-		UsagePtr_t const & pusage(usage_it->second);
-
-		// Resource usage of the current AWM
-		rsrc_tmp_path =	ResourcePathUtils::GetTemplate(rsrc_path);
-		curr_awm_rsrc_usage = curr_awm->ResourceUsageAmount(rsrc_tmp_path);
-		logger->Debug("%s: R{%s} usage: in curr AWM = %lu| in cand AWM = %lu",
-				evl_ent.StrId(), rsrc_tmp_path.c_str(),
-				curr_awm_rsrc_usage, pusage->GetAmount());
-
-		// Get the penalty (per resource type)
-		std::string rsrc_name(ResourcePathUtils::GetNameTemplate(rsrc_path));
-		if (rsrc_name.compare(ResourceNames[MCT_RSRC_PE]) == 0)
-			penalty = penalties[MCT_RSRC_PE];
-		else
-			penalty = penalties[MCT_RSRC_MEM];
-
-		// Target resource usage due to the goal gap assertion
-		target_usage = static_cast<float>(curr_awm_rsrc_usage) *
-			(1.0 + static_cast<float>(ggap) / 100.0);
-		logger->Debug("%s: R{%s} target usage: %.2f", evl_ent.StrId(),
-				rsrc_tmp_path.c_str(), target_usage);
-
-		// Skip the resource whether it satisfies the goal-gap usage requirement
-		if (pusage->GetAmount() >= target_usage)
-			continue;
-
-		/*
-		// Compute the contribute index (with penalization)
-		index = static_cast<float>(evl_ent.pawm->Value()) * (1.0 - penalty)
-			* pusage->GetAmount()
-			/ target_usage;
-		logger->Debug("%s: R{%s} index: %.4f", evl_ent.StrId(),
-				rsrc_tmp_path.c_str(), index);
-*/
-
-
-		// Get the index related to the most penalizing resource usage
-		if (index < ctrib)
-			ctrib = index;
-	}
-#endif
-	logger->Notice("####### %s: AWM Value index: %.4f", evl_ent.StrId(),
-				ctrib);
+	logger->Debug("%s: AWM Value index: %.4f", evl_ent.StrId(),	ctrib);
 	return MCT_SUCCESS;
 }
 
