64743019291e4d16e8cfeca559b74c4c42b7bcdb
Vincenzo Consales <vincenzo.consales@gmail.com>
2012-06-04 22:57:19 +0200

    [RTLIB_AS-RTM 1/6] Add UPDATE_[UPPER|LOWER]_BOUND
    This commit introduces two macros in the code of the AS-RTM that are used
    to update the constraints used to select OperatingPoints.
    As stated as well in the comments, a default action is taken when no
    constraint of the given type (upper or lower) is found: a new constraint
    with a strict inequality. It is just a default implementation: nothing avoids
    to change it and switch to a normal "inequality" (>= vs > and <= vs <)
    
    Signed-off-by: Vincenzo Consales <vincenzo.consales@gmail.com>
    Acked-by: Andrea Di Gesare <andrea.digesare@gmail.com>
    Signed-off-by: Patrick Bellasi <derkling@gmail.com>

--- rtlib/monitors/application_rtrm.cc
+++ rtlib/monitors/application_rtrm.cc
@@ -20,3 +20,49 @@
 void ApplicationRTRM::setGoals(GoalsList &goalsList) {
 	this->goalsList = goalsList;
 }
+/*
+ * The two macros below, check if an upper or lower bound is present in
+ * the opFilters structure and updates it with the new contraint calculated by
+ * the adjustConstraints function. When no bound is found, the macro puts a new
+ * constraint that is > or < even if the goal has been declared with >= or <=
+ * TODO Consider whether to add automatically a constraint when a new goal is
+ * declared. If so, the fix above is not needed anymore because the contraints
+ * will be always present in opFilters. The cons is that if the model of the
+ * application is not exact (even just a proportional) when the application
+ * starts, the AS-RTRM might not find any feasible point. It will know the ratio
+ * between the modeled performance and actual ones only after the first
+ * goal-checking phase. Another possible solution is to avoid checking
+ * non-resource constraints at the first check. We will consider it as well and
+ * take a decision soon.
+ */
+#define UPDATE_UPPER_BOUND()\
+found = false;\
+for (itc = opFilters.begin(); itc!=opFilters.end();++itc){\
+	if (itc->name == metricName && \
+		(itc->cFunction.target_type() ==\
+			ComparisonFunctors::Less.target_type() || \
+		itc->cFunction.target_type() ==\
+			ComparisonFunctors::LessOrEqual.target_type())) {\
+			itc->value = newConstraint; \
+			found = true;\
+	}\
+}\
+if (!found) \
+	opFilters.push_back(OP_Filter(metricName, ComparisonFunctors::Less, \
+				      newConstraint));
+
+#define UPDATE_LOWER_BOUND()\
+found = false;\
+for (itc = opFilters.begin(); itc!=opFilters.end();++itc) {\
+	if (itc->name == metricName && \
+		(itc->cFunction.target_type() ==\
+			ComparisonFunctors::Greater.target_type() || \
+		itc->cFunction.target_type() ==\
+			ComparisonFunctors::GreaterOrEqual.target_type())) { \
+			itc->value = newConstraint;\
+			found = true;\
+		}\
+}\
+if (!found)\
+	opFilters.push_back(OP_Filter(metricName, ComparisonFunctors::Greater, \
+				      newConstraint));
